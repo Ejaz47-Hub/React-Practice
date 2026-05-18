7442cd836d02a85ea0a9f3d0ad83366e32401d88
Ejaz47-Hub <ejaz47026@gmail.com>
2026-05-18 08:39:56 +0530

    Redux Toolkit Project

---
+++ RTK-Project/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full flex justify-center items-center text-blue-500'>App</div>
+  )
+}
+
+export default App
